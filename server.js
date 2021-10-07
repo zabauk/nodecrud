7bcf95954abda4139b74038738b9c54cbd95ce92
Zabauk <mail@zabauk.com>
2021-10-07 09:56:24 -0400

    post number added from env

--- server.js
+++ server.js
@@ -32,5 +32,6 @@ app.use('/api', PostRouter);
 //connect to database
 connectDB();
 
-const port=5000;
+const port=process.env.PORT || 5000;
+
 app.listen(port, ()=>console.log(`Server is running on port ${port}`));
